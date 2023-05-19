a58aa1976c09ec8c3e7912f602dd971603b34053
andres-ali-22 <134099302+andres-ali-22@users.noreply.github.com>
2023-05-19 14:05:24 -0600

    Actualización Ejercicio 6.js

--- ejercicio6.js
+++ ejercicio6.js
@@ -1,1 +1,35 @@
+const { Client } = require("pg");
 
+function getUsers(callback) {
+  const connectionString = process.env.DATABASE_URL;
+  const client = new Client({ connectionString });
+
+  client.connect((connectError) => {
+    if (connectError) {
+      console.error("Error connecting to database:", connectError);
+      callback(connectError, null);
+      client.end();
+      return;
+    }
+
+    const query = "SELECT * FROM users";
+    client.query(query, (queryError, result) => {
+      if (queryError) {
+        console.error("Error running query:", queryError);
+        callback(queryError, null);
+      } else {
+        callback(null, result.rows);
+      }
+      
+      client.end();
+    });
+  });
+}
+
+getUsers((error, users) => {
+  if (error) {
+    console.error("Error retrieving users:", error);
+  } else {
+    console.log("Users:", users);
+  }
+});
